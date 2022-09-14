5b2e597d612c0281a3da556abab3e48218b31fe6
StaceyThandekile <staceythandekile@gmail.com>
2022-09-14 13:00:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* main - print putchar
-* Return: always 0
-*/
+ * main - print putchar
+ * Return: always 0
+ */
 
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
+	
 	return (0);
 
 }

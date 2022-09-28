0927563010075b175422385346879e2cf8cc6aaa
StaceyThandekile <staceythandekile@gmail.com>
2022-09-28 09:08:44 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,31 @@
 #include "main.h"
 /**
+  *helperFunction - returns 0 or 1.
+  *@num: number being checked.
+  *@i: possible factor of the number. 
+  *
+  *Return: 0 if not prime, 1 if prime.
+  */
+int helperFunction(int num, int 1)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (helperFunction(num , i + 1));
+		}
+	}
+	else
+	{
+		return (i);
+	}
+}
+
+/**
   *_sqrt_recursion - returns the natural square root of a number.
   *@n: number to find sqrt of.
   *
@@ -11,6 +37,6 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else 
-		return (helperfunction(n, 0));
+		return (helperFunction(n, 0));
 }
 

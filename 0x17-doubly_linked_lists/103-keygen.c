66157ec7f19b39f328492176c750b988b2a74150
StaceyThandekile <staceythandekile@gmail.com>
2022-12-09 06:53:54 -0800

    tasks

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * f4 - finds the biggest number
+ *
+ * @usrn: username
+ * @len: length of username
+ * Return: the biggest number
+ */
+int f4(char *usrn, int len)
+{
+	int ch;
+	int vch;
+
